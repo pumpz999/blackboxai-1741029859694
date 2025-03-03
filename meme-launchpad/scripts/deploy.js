8ef5699177ced54ba3ef0f4e7682d76e571e2306
pumpz999 <999leadsolutions@gmail.com>
2025-03-03 14:29:52 -0500

    Add meme-launchpad/scripts/deploy.js 1741030192225

---
+++ meme-launchpad/scripts/deploy.js
@@ -0,0 +1,11 @@
+// Deployment script for smart contracts
+async function main() {
+  // Your deployment logic here
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
